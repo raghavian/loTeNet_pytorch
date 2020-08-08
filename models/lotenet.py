db85d33f3544fcf117d7593ae11260e0a66f5f62
Raghav <raghav@di.ku.dk>
2020-08-08 13:12:36 +0200

    Fix for torch2.0 support

--- models/lotenet.py
+++ models/lotenet.py
@@ -20,11 +20,11 @@ class loTeNet(nn.Module):
 		### Squeezing of spatial dimension in first step	
 		self.kScale = 4
 		nCh =  self.kScale**2 * nCh
-		self.input_dim = self.input_dim/self.kScale
+		self.input_dim = self.input_dim // self.kScale
 
 		self.nCh = nCh
 		self.ker = kernel		 
-		iDim = (self.input_dim/(self.ker))
+		iDim = (self.input_dim // (self.ker))
 
 		feature_dim = 2*nCh 
 		
@@ -39,7 +39,7 @@ class loTeNet(nn.Module):
 		self.BN1 = nn.BatchNorm1d(torch.prod(iDim).numpy(),affine=True)
 
 		
-		iDim = iDim/self.ker
+		iDim = iDim // self.ker
 		feature_dim = 2*self.virtual_dim
 		
 		### Second level MPS blocks
@@ -52,7 +52,7 @@ class loTeNet(nn.Module):
 
 		self.BN2 = nn.BatchNorm1d(torch.prod(iDim).numpy(),affine=True)
 
-		iDim = iDim/self.ker
+		iDim = iDim // self.ker
 
 		### Third level MPS blocks
 		self.module3 = nn.ModuleList([ MPS(input_dim=self.ker**2,
@@ -72,15 +72,14 @@ class loTeNet(nn.Module):
 				parallel_eval=parallel_eval)
 		
 	def forward(self,x):
-
 		b = x.shape[0] #Batch size
-		iDim = self.input_dim/(self.ker)
+		iDim = self.input_dim//(self.ker)
 
-        # Increase input feature channel
-        iDim = self.input_dim
-        if self.kScale > 1:
+		# Increase input feature channel
+		iDim = self.input_dim
+		if self.kScale > 1:
 			x = x.unfold(2,iDim[0],iDim[0]).unfold(3,iDim[1],iDim[1])
-			x = x.reshape(b[0],-1,iDim[0],iDim[1])
+			x = x.reshape(b,-1,iDim[0],iDim[1])
 
 		# Level 1 contraction		 
 		x = x.unfold(2,iDim[0],iDim[0]).unfold(3,iDim[1],iDim[1]).reshape(b,
@@ -92,7 +91,7 @@ class loTeNet(nn.Module):
 		# Level 2 contraction
 
 		y = y.view(b,self.virtual_dim,iDim[0],iDim[1])
-		iDim = (iDim/self.ker)
+		iDim = (iDim//self.ker)
 		y = y.unfold(2,iDim[0],iDim[0]).unfold(3,iDim[1],
 				iDim[1]).reshape(b,self.virtual_dim,-1,self.ker**2)
 		x = [ self.module2[i](y[:,:,i]) for i in range(len(self.module2))]
@@ -102,7 +101,7 @@ class loTeNet(nn.Module):
 
 		# Level 3 contraction
 		x = x.view(b,self.virtual_dim,iDim[0],iDim[1])
-		iDim = (iDim/self.ker)
+		iDim = (iDim//self.ker)
 		x = x.unfold(2,iDim[0],iDim[0]).unfold(3,iDim[1],
 				iDim[1]).reshape(b,self.virtual_dim,-1,self.ker**2)
 		y = [ self.module3[i](x[:,:,i]) for i in range(len(self.module3))]
